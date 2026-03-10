3931e1d0298296959a527fcc4fb18668ef261026
arpadav <arpadav@gmail.com>
2026-03-10 17:22:11 -0400

    better logging, fixing url

--- install.sh
+++ install.sh
@@ -8,19 +8,34 @@
 # Author: aav
 # --------------------------------------------------
 set -eu
+NIX_SH="$HOME/.nix-profile/etc/profile.d/nix.sh"
+step() {
+    printf "\033[1;34m=>\033[0m %s...\n" "$1"
+}
 
 # --------------------------------------------------
-# install nix if not present
+# source nix env if it exists but isn't on PATH
 # --------------------------------------------------
 if ! command -v nix >/dev/null 2>&1; then
-    curl -L https://nixos.org/nix/install | sh -s -- --no-daemon --yes
+    if [ -f "$NIX_SH" ]; then
+        . "$NIX_SH"
+    fi
+fi
+# --------------------------------------------------
+# if still not found, actuall install
+# --------------------------------------------------
+if ! command -v nix >/dev/null 2>&1; then
+    step "Installing Nix"
+    curl -L https://nixos.org/nix/install | sh -s -- --no-daemon --yes >/dev/null 2>&1
     . "$HOME/.nix-profile/etc/profile.d/nix.sh"
 fi
 
 # --------------------------------------------------
 # apply aedit via home-manager
 # --------------------------------------------------
+step "Installing aedit"
 nix run home-manager/master -- switch \
-    --flake "github:arpadav/aedit?ref=preparing-for-release?dir=headless#headless" \
+    --flake "github:arpadav/aedit?ref=preparing-for-release&dir=headless#headless" \
     --impure \
-    --extra-experimental-features "nix-command flakes"
+    --extra-experimental-features "nix-command flakes" \
+    2>&1 | tail -1
