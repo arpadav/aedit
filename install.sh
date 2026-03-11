d369adb7ebd7bf3630a7ee8cbb88bb5d2adbacb8
arpadav <arpadav@gmail.com>
2026-03-11 13:05:45 -0400

    using hi.sh

--- install.sh
+++ install.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/usr/bin/env sh
 #
 # aedit headless installer
 #
@@ -7,48 +7,8 @@
 #
 # Author: aav
 # --------------------------------------------------
-set -eu
-NIX_SH="$HOME/.nix-profile/etc/profile.d/nix.sh"
-step() {
-    printf "  \033[1;34m=>\033[0m %s...\n" "$1"
-}
-# --------------------------------------------------
-# source nix env if it exists but isn't on PATH
-# --------------------------------------------------
-step "Checking Nix"
-if ! command -v nix >/dev/null 2>&1; then
-    if [ -f "$NIX_SH" ]; then
-        . "$NIX_SH"
-    fi
-fi
-# --------------------------------------------------
-# if still not found, actuall install
-# --------------------------------------------------
-if ! command -v nix >/dev/null 2>&1; then
-    step "Installing Nix"
-    curl -L https://nixos.org/nix/install | sh -s -- --no-daemon --yes >/dev/null
-    . "$HOME/.nix-profile/etc/profile.d/nix.sh"
-fi
-
-# --------------------------------------------------
-# install aedit
-# --------------------------------------------------
-step "Installing aedit..."
-nix run home-manager/master -- switch \
-    --flake "github:arpadav/aedit?ref=preparing-for-release&dir=headless#headless" \
-    --impure \
-    --no-write-lock-file \
-    --extra-experimental-features "nix-command flakes"
-
-# --------------------------------------------------
-# apply env
-# --------------------------------------------------
-step "Appying env..."
-source ~/.profile 2>&1
-broot --install 2>&1
-
-# --------------------------------------------------
-# done! print usage
-# --------------------------------------------------
-step "Done!"
-echo $(ae --help)
+command -v curl >/dev/null 2>&1 || echo "curl is required but not installed"
+curl -fsSL https://raw.githubusercontent.com/arpadav/home/main/hi.sh | sh -s -- \
+    --runner home-manager/master \
+    --flake "github:arpadav/aedit?dir=headless#headless" \
+    --name "aedit"
